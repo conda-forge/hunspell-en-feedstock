1c5c9c7716b71ad981303806e49f62143c6adb2f
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-01-12 09:29:29 -0600

    more defensive copy

--- recipe/copy_files.py
+++ recipe/copy_files.py
@@ -1,19 +1,37 @@
 import os
 import shutil
+import sys
 from pathlib import Path
 
 SRC = Path(os.environ["SRC_DIR"])
 OUT = Path(os.environ["PREFIX"]) / "share/hunspell_dictionaries"
 
-OUT.mkdir(exist_ok=True, parents=True)
-
 PKG = os.environ["PKG_NAME"]
 L10N = PKG.split("-")[-1].upper()
 PATH = next(SRC.glob(f"en_{L10N}*"))
+GLOBS = ["*.aff", "*.dic"]
 
-print("Copying files to", OUT)
 
-for glob in ["*.aff", "*.dic"]:
-    for path in sorted(PATH.glob(glob)):
+def copy_files():
+    if not PATH.exists():
+        print(PATH, "does not exist")
+        return 1
+
+    print("Copying files to", OUT)
+
+    all_files = sorted(sum([[*PATH.glob(glob)] for glob in GLOBS], []))
+
+    if not all_files:
+        return 1
+
+    OUT.mkdir(exist_ok=True, parents=True)
+
+    for path in all_files:
         print("...", path.name)
         shutil.copy2(path, OUT / path.name)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(copy_files())
